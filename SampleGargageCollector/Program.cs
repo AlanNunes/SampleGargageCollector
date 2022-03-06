e8111d4933e9176666d350bd0564fac9b039ee66
alannunes <alann.625@gmail.com>
2022-03-06 10:43:16 -0300

    Add project

---
+++ SampleGargageCollector/Program.cs
@@ -0,0 +1,15 @@
+﻿using SampleGargageCollector;
+using System.Diagnostics;
+
+var watch = Stopwatch.StartNew();
+
+var calc = new Calculator();
+for (var i = 0; i < 1_000_000_000; i++)
+{
+    calc.Sum(10, i);
+}
+
+watch.Stop();
+var coletasDeLixo = GC.CollectionCount(0) + GC.CollectionCount(1) + GC.CollectionCount(2);
+Console.WriteLine($"Time: {watch.ElapsedMilliseconds} ms");
+Console.WriteLine($"GC: {coletasDeLixo}");
---
+++ SampleGargageCollector/StaticCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace SampleGargageCollector
+{
+    public class StaticCalculator
+    {
+        public static int Sum(int a, int b) => a + b;
+        public static int Minius(int a, int b) => a - b;
+        public static int Mult(int a, int b) => a * b;
+        public static int Div(int a, int b) => a / b;
+    }
+}
